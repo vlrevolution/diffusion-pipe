d334cbdbfa9cf2c3fb317f8aaf84ca8302bb0bf4
VLRevolution <dreamer@vlrevolution.com>
2025-10-01 09:09:14 +0300

    support different adapter libraries

--- models/base.py
+++ models/base.py
@@ -11,8 +11,14 @@ import torchvision
 from PIL import Image, ImageOps
 from torchvision import transforms
 import imageio
+from lycoris import create_lycoris, LycorisNetwork
 
-from utils.common import is_main_process, VIDEO_EXTENSIONS, round_to_nearest_multiple, round_down_to_multiple
+from utils.common import (
+    is_main_process,
+    VIDEO_EXTENSIONS,
+    round_to_nearest_multiple,
+    round_down_to_multiple,
+)
 
 
 def make_contiguous(*tensors):
@@ -24,49 +30,63 @@ def extract_clips(video, target_frames, video_clip_mode):
     frames = video.shape[1]
     if frames < target_frames:
         # TODO: think about how to handle this case. Maybe the video should have already been thrown out?
-        print(f'video with shape {video.shape} is being skipped because it has less than the target_frames')
+        print(
+            f"video with shape {video.shape} is being skipped because it has less than the target_frames"
+        )
         return []
 
-    if video_clip_mode == 'single_beginning':
+    if video_clip_mode == "single_beginning":
         return [video[:, :target_frames, ...]]
-    elif video_clip_mode == 'single_middle':
+    elif video_clip_mode == "single_middle":
         start = int((frames - target_frames) / 2)
-        assert frames-start >= target_frames
-        return [video[:, start:start+target_frames, ...]]
-    elif video_clip_mode == 'multiple_overlapping':
+        assert frames - start >= target_frames
+        return [video[:, start : start + target_frames, ...]]
+    elif video_clip_mode == "multiple_overlapping":
         # Extract multiple clips so we use the whole video for training.
         # The clips might overlap a little bit. We never cut anything off the end of the video.
         num_clips = ((frames - 1) // target_frames) + 1
-        start_indices = torch.linspace(0, frames-target_frames, num_clips).int()
-        return [video[:, i:i+target_frames, ...] for i in start_indices]
+        start_indices = torch.linspace(0, frames - target_frames, num_clips).int()
+        return [video[:, i : i + target_frames, ...] for i in start_indices]
     else:
-        raise NotImplementedError(f'video_clip_mode={video_clip_mode} is not recognized')
+        raise NotImplementedError(
+            f"video_clip_mode={video_clip_mode} is not recognized"
+        )
 
 
 def convert_crop_and_resize(pil_img, width_and_height):
-    if pil_img.mode not in ['RGB', 'RGBA'] and 'transparency' in pil_img.info:
-        pil_img = pil_img.convert('RGBA')
+    if pil_img.mode not in ["RGB", "RGBA"] and "transparency" in pil_img.info:
+        pil_img = pil_img.convert("RGBA")
 
     # add white background for transparent images
-    if pil_img.mode == 'RGBA':
-        canvas = Image.new('RGBA', pil_img.size, (255, 255, 255))
+    if pil_img.mode == "RGBA":
+        canvas = Image.new("RGBA", pil_img.size, (255, 255, 255))
         canvas.alpha_composite(pil_img)
-        pil_img = canvas.convert('RGB')
+        pil_img = canvas.convert("RGB")
     else:
-        pil_img = pil_img.convert('RGB')
+        pil_img = pil_img.convert("RGB")
 
     return ImageOps.fit(pil_img, width_and_height)
 
 
 class PreprocessMediaFile:
-    def __init__(self, config, support_video=False, framerate=None, round_height=16, round_width=16, round_frames=4):
+    def __init__(
+        self,
+        config,
+        support_video=False,
+        framerate=None,
+        round_height=16,
+        round_width=16,
+        round_frames=4,
+    ):
         self.config = config
-        self.video_clip_mode = config.get('video_clip_mode', 'single_beginning')
-        print(f'using video_clip_mode={self.video_clip_mode}')
-        self.pil_to_tensor = transforms.Compose([transforms.ToTensor(), transforms.Normalize([0.5], [0.5])])
+        self.video_clip_mode = config.get("video_clip_mode", "single_beginning")
+        print(f"using video_clip_mode={self.video_clip_mode}")
+        self.pil_to_tensor = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
+        )
         self.support_video = support_video
         self.framerate = framerate
-        print(f'using framerate={self.framerate}')
+        print(f"using framerate={self.framerate}")
         self.round_height = round_height
         self.round_width = round_width
         self.round_frames = round_frames
@@ -79,14 +99,16 @@ class PreprocessMediaFile:
             tar_f.close()
 
     def __call__(self, spec, mask_filepath, size_bucket=None):
-        is_video = (Path(spec[1]).suffix in VIDEO_EXTENSIONS)
+        is_video = Path(spec[1]).suffix in VIDEO_EXTENSIONS
 
         if spec[0] is None:
             tar_f = None
             filepath_or_file = str(spec[1])
         else:
             tar_filename = spec[0]
-            tar_f = self.tarfile_map.setdefault(tar_filename, tarfile.TarFile(tar_filename))
+            tar_f = self.tarfile_map.setdefault(
+                tar_filename, tarfile.TarFile(tar_filename)
+            )
             filepath_or_file = tar_f.extractfile(str(spec[1]))
 
         if is_video:
@@ -105,25 +127,35 @@ class PreprocessMediaFile:
         if size_bucket is not None:
             size_bucket_width, size_bucket_height, size_bucket_frames = size_bucket
         else:
-            size_bucket_width, size_bucket_height, size_bucket_frames = width, height, num_frames
+            size_bucket_width, size_bucket_height, size_bucket_frames = (
+                width,
+                height,
+                num_frames,
+            )
 
-        height_rounded = round_to_nearest_multiple(size_bucket_height, self.round_height)
+        height_rounded = round_to_nearest_multiple(
+            size_bucket_height, self.round_height
+        )
         width_rounded = round_to_nearest_multiple(size_bucket_width, self.round_width)
-        frames_rounded = round_down_to_multiple(size_bucket_frames - 1, self.round_frames) + 1
+        frames_rounded = (
+            round_down_to_multiple(size_bucket_frames - 1, self.round_frames) + 1
+        )
         resize_wh = (width_rounded, height_rounded)
 
         if mask_filepath:
-            mask_img = Image.open(mask_filepath).convert('RGB')
+            mask_img = Image.open(mask_filepath).convert("RGB")
             img_hw = (height, width)
             mask_hw = (mask_img.height, mask_img.width)
             if mask_hw != img_hw:
                 raise ValueError(
-                    f'Mask shape {mask_hw} was not the same as image shape {img_hw}.\n'
-                    f'Image path: {spec[1]}\n'
-                    f'Mask path: {mask_filepath}'
+                    f"Mask shape {mask_hw} was not the same as image shape {img_hw}.\n"
+                    f"Image path: {spec[1]}\n"
+                    f"Mask path: {mask_filepath}"
                 )
             mask_img = ImageOps.fit(mask_img, resize_wh)
-            mask = torchvision.transforms.functional.to_tensor(mask_img)[0].to(torch.float16)  # use first channel
+            mask = torchvision.transforms.functional.to_tensor(mask_img)[0].to(
+                torch.float16
+            )  # use first channel
         else:
             mask = None
 
@@ -134,7 +166,7 @@ class PreprocessMediaFile:
             cropped_image = convert_crop_and_resize(frame, resize_wh)
             resized_video[i, ...] = self.pil_to_tensor(cropped_image)
 
-        if hasattr(filepath_or_file, 'close'):
+        if hasattr(filepath_or_file, "close"):
             filepath_or_file.close()
 
         if not self.support_video:
@@ -162,6 +194,30 @@ class BasePipeline:
         raise NotImplementedError()
 
     def configure_adapter(self, adapter_config):
+        """
+        Configures and applies an adapter to the model's transformer.
+        This method acts as a dispatcher based on the adapter configuration.
+        """
+        adapter_library = adapter_config.get("library", "peft").lower()
+
+        if adapter_library == "peft":
+            self._create_peft_adapter(adapter_config)
+        elif adapter_library == "lycoris":
+            self._create_lycoris_adapter(adapter_config)
+        else:
+            raise NotImplementedError(
+                f"Adapter library '{adapter_library}' is not supported."
+            )
+
+        # Set dtypes and original_name for all parameters, regardless of library.
+        for name, p in self.transformer.named_parameters():
+            if not hasattr(p, "original_name"):
+                p.original_name = name
+            if p.requires_grad:
+                p.data = p.data.to(adapter_config["dtype"])
+
+    def _create_peft_adapter(self, adapter_config):
+        """Creates and applies a PEFT adapter (e.g., LoRA)."""
         target_linear_modules = set()
         for name, module in self.transformer.named_modules():
             if module.__class__.__name__ not in self.adapter_target_modules:
@@ -171,47 +227,91 @@ class BasePipeline:
                     target_linear_modules.add(full_submodule_name)
         target_linear_modules = list(target_linear_modules)
 
-        adapter_type = adapter_config['type']
-        if adapter_type == 'lora':
+        adapter_type = adapter_config.get("type", "lora").lower()
+        if adapter_type == "lora":
             peft_config = peft.LoraConfig(
-                r=adapter_config['rank'],
-                lora_alpha=adapter_config['alpha'],
-                lora_dropout=adapter_config['dropout'],
-                bias='none',
-                target_modules=target_linear_modules
+                r=adapter_config["rank"],
+                lora_alpha=adapter_config["alpha"],
+                lora_dropout=adapter_config.get("dropout", 0.0),
+                bias="none",
+                target_modules=target_linear_modules,
             )
         else:
-            raise NotImplementedError(f'Adapter type {adapter_type} is not implemented')
-        self.peft_config = peft_config
-        self.lora_model = peft.get_peft_model(self.transformer, peft_config)
-        if is_main_process():
-            self.lora_model.print_trainable_parameters()
-        for name, p in self.transformer.named_parameters():
-            p.original_name = name
-            if p.requires_grad:
-                p.data = p.data.to(adapter_config['dtype'])
+            raise NotImplementedError(
+                f"PEFT adapter type '{adapter_type}' is not implemented."
+            )
 
-    def save_adapter(self, save_dir, peft_state_dict):
-        raise NotImplementedError()
+        self.peft_config = peft_config
+        self.adapter_model = peft.get_peft_model(self.transformer, peft_config)
+        if is_main_process():
+            self.adapter_model.print_trainable_parameters()
+
+    def _create_lycoris_adapter(self, adapter_config):
+        """Creates and applies a LyCORIS adapter (e.g., LoKR)."""
+        # LyCORIS uses presets to define which modules to target.
+        # We will set a preset that targets the modules defined in `adapter_target_modules`.
+        LycorisNetwork.apply_preset(
+            {
+                "target_module": self.adapter_target_modules,
+            }
+        )
+
+        self.adapter_model = create_lycoris(
+            self.transformer,
+            multiplier=1.0,
+            linear_dim=adapter_config.get("rank", 4),
+            linear_alpha=adapter_config.get("alpha", 1.0),
+            algo=adapter_config.get("type", "lora"),
+            **adapter_config.get("network_args", {}),
+        )
+        self.adapter_model.apply_to()
+
+    def save_adapter(self, save_dir, state_dict):
+        adapter_library = self.config["adapter"].get("library", "peft").lower()
+
+        if adapter_library == "peft":
+            # Default PEFT saving logic
+            self.peft_config.save_pretrained(save_dir)
+            safetensors.torch.save_file(
+                state_dict,
+                save_dir / "adapter_model.safetensors",
+                metadata={"format": "pt"},
+            )
+        elif adapter_library == "lycoris":
+            # LyCORIS saving logic
+            comfyui_state_dict = {
+                "diffusion_model." + k: v for k, v in state_dict.items()
+            }
+            safetensors.torch.save_file(
+                comfyui_state_dict,
+                save_dir / "adapter_model.safetensors",
+                metadata={"format": "pt"},
+            )
+        else:
+            raise NotImplementedError(
+                f"Saving for adapter library '{adapter_library}' is not supported."
+            )
 
     def load_adapter_weights(self, adapter_path):
         if is_main_process():
-            print(f'Loading adapter weights from path {adapter_path}')
-        safetensors_files = list(Path(adapter_path).glob('*.safetensors'))
+            print(f"Loading adapter weights from path {adapter_path}")
+        safetensors_files = list(Path(adapter_path).glob("*.safetensors"))
         if len(safetensors_files) == 0:
-            raise RuntimeError(f'No safetensors file found in {adapter_path}')
+            raise RuntimeError(f"No safetensors file found in {adapter_path}")
         if len(safetensors_files) > 1:
-            raise RuntimeError(f'Multiple safetensors files found in {adapter_path}')
+            raise RuntimeError(f"Multiple safetensors files found in {adapter_path}")
         adapter_state_dict = safetensors.torch.load_file(safetensors_files[0])
         modified_state_dict = {}
         model_parameters = set(name for name, p in self.transformer.named_parameters())
         for k, v in adapter_state_dict.items():
             # Replace Diffusers or ComfyUI prefix
-            k = re.sub(r'^(transformer|diffusion_model)\.', '', k)
+            k = re.sub(r"^(transformer|diffusion_model)\.", "", k)
             # Replace weight at end for LoRA format
-            k = re.sub(r'\.weight$', '.default.weight', k)
+            k = re.sub(r"\.weight$", ".default.weight", k)
             if k not in model_parameters:
-                raise RuntimeError(f'modified_state_dict key {k} is not in the model parameters')
+                raise RuntimeError(
+                    f"modified_state_dict key {k} is not in the model parameters"
+                )
             modified_state_dict[k] = v
         self.transformer.load_state_dict(modified_state_dict, strict=False)
 
@@ -245,30 +345,31 @@ class BasePipeline:
     # Get param groups that will be passed into the optimizer. Models can override this, e.g. SDXL
     # supports separate learning rates for unet and text encoders.
     def get_param_groups(self, parameters):
-        return [{'params': parameters}]
+        return [{"params": parameters}]
 
     # Default loss_fn. MSE between output and target, with mask support.
     def get_loss_fn(self):
         def loss_fn(output, label):
             target, mask = label
-            with torch.autocast('cuda', enabled=False):
+            with torch.autocast("cuda", enabled=False):
                 output = output.to(torch.float32)
                 target = target.to(output.device, torch.float32)
-                if 'pseudo_huber_c' in self.config:
-                    c = self.config['pseudo_huber_c']
-                    loss = torch.sqrt((output-target)**2 + c**2) - c
+                if "pseudo_huber_c" in self.config:
+                    c = self.config["pseudo_huber_c"]
+                    loss = torch.sqrt((output - target) ** 2 + c**2) - c
                 else:
-                    loss = F.mse_loss(output, target, reduction='none')
+                    loss = F.mse_loss(output, target, reduction="none")
                 # empty tensor means no masking
                 if mask.numel() > 0:
                     mask = mask.to(output.device, torch.float32)
                     loss *= mask
                 loss = loss.mean()
             return loss
+
         return loss_fn
 
     def enable_block_swap(self, blocks_to_swap):
-        raise NotImplementedError('Block swapping is not implemented for this model')
+        raise NotImplementedError("Block swapping is not implemented for this model")
 
     def prepare_block_swap_training(self):
         pass
--- utils/saver.py
+++ utils/saver.py
@@ -13,21 +13,26 @@ from utils.common import is_main_process
 
 def convert_state_dict_dtype(state_dict, dtype):
     for key, v in state_dict.items():
-        state_dict[key] = v.to(device='cpu', dtype=dtype)
+        state_dict[key] = v.to(device="cpu", dtype=dtype)
 
 
 last_checkpoint_time = None
+
+
 def need_to_checkpoint(config, epoch=None):
     global last_checkpoint_time
 
     if epoch is not None:
-        if 'checkpoint_every_n_epochs' in config and epoch % config['checkpoint_every_n_epochs'] == 0:
+        if (
+            "checkpoint_every_n_epochs" in config
+            and epoch % config["checkpoint_every_n_epochs"] == 0
+        ):
             last_checkpoint_time = time.time()
             return True
         else:
             return False
 
-    if 'checkpoint_every_n_minutes' not in config:
+    if "checkpoint_every_n_minutes" not in config:
         return False
 
     checkpoint = False
@@ -36,7 +41,9 @@ def need_to_checkpoint(config, epoch=None):
         current_time = time.time()
         if last_checkpoint_time is None:
             last_checkpoint_time = current_time
-        elif (current_time - last_checkpoint_time) / 60 > config['checkpoint_every_n_minutes']:
+        elif (current_time - last_checkpoint_time) / 60 > config[
+            "checkpoint_every_n_minutes"
+        ]:
             checkpoint = True
             last_checkpoint_time = current_time
     result = [checkpoint]
@@ -45,7 +52,17 @@ def need_to_checkpoint(config, epoch=None):
 
 
 class Saver:
-    def __init__(self, args, config, is_adapter, save_root, model, train_dataloader, model_engine, pipeline_model):
+    def __init__(
+        self,
+        args,
+        config,
+        is_adapter,
+        save_root,
+        model,
+        train_dataloader,
+        model_engine,
+        pipeline_model,
+    ):
         self.args = args
         self.config = config
         self.is_adapter = is_adapter
@@ -59,57 +76,56 @@ class Saver:
         dp_id = self.model_engine.grid.get_data_parallel_rank()
         stage_id = self.model_engine.grid.get_pipe_parallel_rank()
         save_dir = self.save_root / name
-        tmp_dir = save_dir / 'tmp'
+
+        # Only the master process on the first stage saves the model.
         if dp_id == 0 and stage_id == 0:
-            os.makedirs(tmp_dir, exist_ok=False)
-        dist.barrier()
-        if dp_id == 0:
-            partial_state_dict = {}
-            for name, p in self.pipeline_model.named_parameters():
-                if p.requires_grad:
-                    if not hasattr(p, 'original_name'):
-                        logger.warning(f'WARNING: parameter {name} requires_grad but does not have original_name. Not saving it.')
-                        continue
-                    # TODO: maybe this needs to change if we ever have non-lora adapters?
-                    partial_state_dict[p.original_name.replace('.default', '').replace('.modules_to_save', '')] = p.detach()
-                    if 'save_dtype' in self.config:
-                        convert_state_dict_dtype(partial_state_dict, self.config['save_dtype'])
-            torch.save(partial_state_dict, tmp_dir / f'state_dict_{stage_id}.bin')
-        dist.barrier()
-        if dp_id == 0 and stage_id == 0:
-            state_dict = {}
-            for path in tmp_dir.glob('*.bin'):
-                state_dict.update(torch.load(path, weights_only=True, map_location='cpu'))
+            os.makedirs(save_dir, exist_ok=True)
+
+            # The adapter_model (PEFT or LyCORIS) lives on the model object.
+            # Both libraries provide a state_dict() method that returns only the adapter weights.
+            state_dict = self.model.adapter_model.state_dict()
+
+            if "save_dtype" in self.config:
+                convert_state_dict_dtype(state_dict, self.config["save_dtype"])
+
             self.model.save_adapter(save_dir, state_dict)
             shutil.copy(self.args.config, save_dir)
-            shutil.rmtree(tmp_dir)
+
+        # Barrier to ensure all processes wait for the save to complete.
+        dist.barrier()
 
     def save_full_model(self, name):
         dp_id = self.model_engine.grid.get_data_parallel_rank()
         stage_id = self.model_engine.grid.get_pipe_parallel_rank()
         save_dir = self.save_root / name
-        tmp_dir = save_dir / 'tmp'
+        tmp_dir = save_dir / "tmp"
         if dp_id == 0 and stage_id == 0:
             os.makedirs(tmp_dir, exist_ok=False)
         dist.barrier()
         if dp_id == 0:
             # With BF16_Optimizer, we get pickle errors unless we do p.detach(). I have no idea why.
-            partial_state_dict = {p.original_name: p.detach() for p in self.pipeline_model.parameters() if hasattr(p, 'original_name')}
-            if 'save_dtype' in self.config:
-                convert_state_dict_dtype(partial_state_dict, self.config['save_dtype'])
-            torch.save(partial_state_dict, tmp_dir / f'state_dict_{stage_id}.bin')
+            partial_state_dict = {
+                p.original_name: p.detach()
+                for p in self.pipeline_model.parameters()
+                if hasattr(p, "original_name")
+            }
+            if "save_dtype" in self.config:
+                convert_state_dict_dtype(partial_state_dict, self.config["save_dtype"])
+            torch.save(partial_state_dict, tmp_dir / f"state_dict_{stage_id}.bin")
         dist.barrier()
         if dp_id == 0 and stage_id == 0:
             state_dict = {}
-            for path in tmp_dir.glob('*.bin'):
-                state_dict.update(torch.load(path, map_location='cpu', weights_only=True))
+            for path in tmp_dir.glob("*.bin"):
+                state_dict.update(
+                    torch.load(path, map_location="cpu", weights_only=True)
+                )
             self.model.save_model(save_dir, state_dict)
             shutil.copy(self.args.config, save_dir)
             shutil.rmtree(tmp_dir)
 
     def save_model(self, name):
         if is_main_process():
-            print(f'Saving model to directory {name}')
+            print(f"Saving model to directory {name}")
         if self.is_adapter:
             self.save_adapter(name)
         else:
@@ -119,12 +135,12 @@ class Saver:
         self.model_engine.save_checkpoint(
             self.save_root,
             client_state={
-                'step': step,
-                'examples': examples,
-                'custom_loader': self.train_dataloader.state_dict(),
+                "step": step,
+                "examples": examples,
+                "custom_loader": self.train_dataloader.state_dict(),
             },
             save_latest=True,
-            exclude_frozen_parameters=True
+            exclude_frozen_parameters=True,
         )
 
     def process_epoch(self, epoch, step, examples):
@@ -133,14 +149,17 @@ class Saver:
             if need_to_checkpoint(self.config, epoch):
                 self.save_checkpoint(step, examples)
                 checkpointed = True
-            if 'save_every_n_epochs' in self.config and epoch % self.config['save_every_n_epochs'] == 0:
-                self.save_model(f'epoch{epoch}')
+            if (
+                "save_every_n_epochs" in self.config
+                and epoch % self.config["save_every_n_epochs"] == 0
+            ):
+                self.save_model(f"epoch{epoch}")
                 saved = True
             epoch = self.train_dataloader.epoch
-            if epoch > self.config['epochs']:
+            if epoch > self.config["epochs"]:
                 return None, checkpointed, saved
             if is_main_process():
-                print(f'Started new epoch: {epoch}')
+                print(f"Started new epoch: {epoch}")
         return epoch, checkpointed, saved
 
     def process_step(self, step, examples):
@@ -148,8 +167,8 @@ class Saver:
         # Look at some simple "signal files" the user can write to save and optionally quit manually
         should_manually_save = False
         should_manually_quit = False
-        save_signal_file = self.save_root / 'save'
-        save_quit_signal_file = self.save_root / 'save_quit'
+        save_signal_file = self.save_root / "save"
+        save_quit_signal_file = self.save_root / "save_quit"
         if save_signal_file.exists() and save_signal_file.is_file():
             should_manually_save = True
             dist.barrier()
@@ -162,8 +181,11 @@ class Saver:
             if is_main_process():
                 os.remove(save_quit_signal_file)
 
-        if 'save_every_n_steps' in self.config and step % self.config['save_every_n_steps'] == 0:
-            self.save_model(f'step{step}')
+        if (
+            "save_every_n_steps" in self.config
+            and step % self.config["save_every_n_steps"] == 0
+        ):
+            self.save_model(f"step{step}")
             saved = True
 
         if need_to_checkpoint(self.config) or should_manually_save:
@@ -171,7 +193,7 @@ class Saver:
             checkpointed = True
 
         if should_manually_quit:
-            print('Manually quitting')
+            print("Manually quitting")
             sys.exit()
 
         return checkpointed, saved
